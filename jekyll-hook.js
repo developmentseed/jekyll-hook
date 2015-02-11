34b8379f75d16580d84dad9ce213d15fdacb206e
Scisco <scisco7@gmail.com>
2015-02-11 15:01:53 -0500

    read config from environment variables

--- jekyll-hook.js
+++ jekyll-hook.js
@@ -2,25 +2,39 @@ var githubhook = require('githubhook');
 var join = require('path').join;
 var fse = require('fs-extra');
 var async = require('async');
-var config  = require('./config.json');
+var env = require('node-env-file');
+
+
 var build = require('./lib/build.js');
 var publish = require('./lib/publish.js');
 var email = require('./lib/email.js');
 
 
-var repo = config.repoName;
+// Read env variables from .env FILE if it exists
+var envFile = join(__dirname, '.env');
 
-var githubHook = config.githubHook || '/hooks/jekyll';
-var githubPort = process.env.PORT || '8080';
+if (fse.existsSync(envFile)) {
+  try {
+    env(__dirname + '/.env');
+  }
+  catch(err) {
+    console.log(err);
+  }
+}
+
+// Read config file after the env variables are recorded
+var config  = require('./lib/config.js');
+
+var repo = config.repoName;
+var tmp = config.temp;
+
 var github = githubhook({
-  path: githubHook,
-  port: githubPort,
+  path: config.githubHook,
+  port: config.port,
   secret: config.secret,
   logger: console
 });
 
-
-var tmp = config.temp || join(__dirname, 'tmp');
 
 // Create tmp directory if doesn't exist
 fse.mkdirs(tmp, function (err) {
@@ -48,12 +62,12 @@ github.on('push:' + repo , function (ref, data) {
       if (config.copyDir) {
         publish.copy(buildDir, config.copyDir, repo, callback);
       }
-      else if (config.s3.isActivated) {
+      if (config.s3.isActivated) {
         publish.s3(config.s3, buildDir, callback);
       }
-      else {
-        callback();
-      }
+
+      callback();
+
     }],
   function(err) {
     if (err) {
